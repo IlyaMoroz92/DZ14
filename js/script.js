1ee46f6170637bee8c209926fab79e7a6358650a
Ilya Moroz <mermermer@inbox.ru>
2022-01-24 18:53:46 +0300

    +js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
-
-function arr(num){
+function arr (num) {
     document.querySelector('p').remove()
     num = Number(document.querySelector('.input-text').value);
     let array = new Array(num+1);
-    for(let i = array.length - 1; i > 0; i--){
-        rand = Math.floor(Math.random() * (i - 1));
-        x = array[rand] || rand;
-        array[rand] = array[i] || i;
-        array[i] = x
-    }
-    let body = document.querySelector('body')
-    const p = document.createElement('p');
-    p.innerHTML = '';
-    p.innerText = '[' + array.toString().split(',').join(',  ') + ']';
-    body.appendChild(p);
-}   
-
-
-
-
+    for (let i = 1; i<(arr.length+1); i++) {
+    let num = Math.floor(Math.random()*(arr.length));
+    if (arr[num] === undefined) {
+        arr[num] =  i;
+        console.log(i);
+        console.log(arr);
+        } else {
+        for ( j = 0; j < (arr.length+1); j++) {
+            if (arr[j] === undefined) {
+            arr[num] =  i;
+            /* arr[num]; */
+            console.log(arr[num]);
+            break;
+            }
+        }       
+        console.log(arr);
+        }
+   }
+   let body = document.querySelector('body')
+   const p = document.createElement('p');
+   p.innerHTML = '';
+   p.innerText = '[' + array.toString().split(',').join(',  ') + ']';
+   body.appendChild(p);
+}
