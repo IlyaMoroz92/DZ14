aed2f3c4236fd5e4edaafb390065af288dd7bae1
Ilya Moroz <mermermer@inbox.ru>
2022-01-24 18:59:26 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,28 +1,24 @@
 function arr (num) {
     document.querySelector('p').remove()
     num = Number(document.querySelector('.input-text').value);
-    let array = new Array(num+1);
+    let arr = new Array(num+1);
     for (let i = 1; i<(arr.length+1); i++) {
     let num = Math.floor(Math.random()*(arr.length));
     if (arr[num] === undefined) {
         arr[num] =  i;
-        console.log(i);
-        console.log(arr);
         } else {
         for ( j = 0; j < (arr.length+1); j++) {
             if (arr[j] === undefined) {
             arr[num] =  i;
-            /* arr[num]; */
             console.log(arr[num]);
             break;
             }
         }       
-        console.log(arr);
         }
    }
    let body = document.querySelector('body')
    const p = document.createElement('p');
    p.innerHTML = '';
-   p.innerText = '[' + array.toString().split(',').join(',  ') + ']';
+   p.innerText = '[' + arr.toString().split(',').join(',  ') + ']';
    body.appendChild(p);
 }
